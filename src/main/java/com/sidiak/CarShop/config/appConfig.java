238b62899baa6e551b0607bf85386589dfe17ca5
Viktor <viktor.sidiak@gmail.com>
2020-06-19 03:50:12 +0300

    fixing problems with starting

--- src/main/java/com/sidiak/CarShop/config/appConfig.java
+++ src/main/java/com/sidiak/CarShop/config/appConfig.java
@@ -2,8 +2,14 @@ package com.sidiak.CarShop.config;
 
 import javax.validation.Constraint;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableAutoConfiguration
+@ComponentScan({"com.sidiak.CarShop"})
+@EnableJpaRepositories({"com.sidiak.CarShop"})
 public class appConfig {
 }
